569933b5171bf179472a1b70864bdbd6c35ea005
s2 <s@ireoo.com>
2014-08-31 04:20:17 +0800

    更新程序

--- include/foot.php
+++ include/foot.php
@@ -13,6 +13,10 @@
     <div class="zhong">
 
         <a style="padding: 0 5px 0 0;" href="http://zhubo.pro" title="星主播">星主播<sup>pro</sup></a>由<a style="padding: 0 5px;" href="http://ireoo.com" title="琦益网">淮安万达信息科技有限公司(ireoo.inc)</a>独家提供全部的技术支持.
+        <script type="text/javascript">
+            var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+            document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F1678be69dbeada46457422cce28b9191' type='text/javascript'%3E%3C/script%3E"));
+        </script>
 
     </div>
 
